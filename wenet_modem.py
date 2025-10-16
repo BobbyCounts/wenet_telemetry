ed3a903e596061fbbee6e2f3326cb5d67ac58022
Bobby Counts <bobby@bobbycounts.com>
2025-10-15 22:34:30 -0500

    Add modem and udp client

--- wenet_modem.py
+++ wenet_modem.py
@@ -8,10 +8,18 @@ import logging
 import json
 from datetime import datetime, timezone
 import sys
+import cbor2
 
-def build_json_payload(string_payload):
-    assert(len(string_payload) <= 252)
-    return json.dumps({'type': 'WENET_TX_TEXT', 'packet': string_payload})
+def build_json_payload(payload: bytes) -> str:
+    if len(payload) > 254:
+        logger.error("Payload too long when building JSON")
+        return ""
+    # payload = payload.ljust(254, b'\x00')
+    # assert(len(payload) == 254)
+    return json.dumps({'type': 'WENET_TX_SEC_PAYLOAD',
+                        'id': 55,
+                        'repeats': 1,
+                        'packet': list(payload)})
 
 parser = argparse.ArgumentParser(description="Wenet UART reader")
 parser.add_argument('--baudrate', type=int, default=460800, help='Baudrate for UART communication')
@@ -46,22 +54,32 @@ while True:
             sys.exit(1)
 
     logger.info("Connecting")
-    with serial_for_url(url, baudrate=args.baudrate) as serial_port:
+    with serial_for_url(url, baudrate=args.baudrate, timeout=0.2) as serial_port:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        input_buffer = bytearray()
         while True:
             try:
-                line = serial_port.readline().decode('utf-8').strip()
+                input_buffer += serial_port.read(64)
             except Exception as e:
                 logger.error(f"Error reading from serial port: {e}")
                 break
-            if "BLE Data: " in line:
-                line = line.split("BLE Data: ")[1]
-                # Insert timestamp
-                line = f"{datetime.now(timezone.utc).isoformat()},{line}"
-                logger.info(f"Data Recieved: {line}")
-                sock.sendto(build_json_payload(line).encode('utf-8'), (args.ip_address, args.port))
-            else:
-                logger.info(line)
+            if b'\n' in input_buffer:
+                # Handle regular log message
+                line_end = input_buffer.index(b'\n')
+                line = input_buffer[:line_end].decode(errors='ignore').strip()
+                input_buffer = input_buffer[line_end+1:]
+                if "BLE RX" in line:
+                    line = line.split("BLE RX: ",1)[1]
+                    data = bytearray.fromhex(line)
+                    logger.info(f"BLE RX data: {cbor2.loads(data)}")
+                    # Send data to UDP server
+                    try:
+                        sock.sendto(build_json_payload(data).encode(), (args.ip_address, args.port))
+                    except Exception as e:
+                        logger.error(f"Error sending UDP packet: {e}")
+                else:
+                    logger.info(line)
+
     
     # Connection closed, try to reconnect
     logger.info("Connection closed, rescanning...")
